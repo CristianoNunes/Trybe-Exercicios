b6641596a53291d79c7423671294c7715d14da0e
CristianoNunes <cristianonunes99@gmail.com>
2020-07-14 18:12:36 -0300

    Bloco4 Dia02 Exercicio3

---
+++ Bloco4/exercicio3.js
@@ -0,0 +1,8 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let sum = 0;
+let average = 0;
+for(let position = 0; position < numbers.length; position += 1){
+    sum += numbers[position];
+}
+average = (sum / numbers.length);
+console.log(average);

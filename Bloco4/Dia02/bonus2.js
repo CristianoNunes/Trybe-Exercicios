a20d92673a4a399965da284210de2e28fe931fc5
CristianoNunes <cristianonunes99@gmail.com>
2020-07-14 20:55:25 -0300

    Bloco4 Dia02 Bonus2

---
+++ Bloco4/Dia02/bonus2.js
@@ -0,0 +1,17 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+for (let i = 1; i < numbers.length; i+= 1) {
+
+    for (let j = 0; j < i; j+= 1) {
+
+        if (numbers[j] < numbers[i]) {
+            let position = numbers[i];
+    
+            numbers[i] = numbers[j];
+            numbers[j] = position;
+        }
+
+    }
+
+}
+console.log(numbers);

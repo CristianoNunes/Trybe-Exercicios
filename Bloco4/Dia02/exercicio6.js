20ee3aee1e8dd5277483d868c6a4e28e46caccd9
CristianoNunes <cristianonunes99@gmail.com>
2020-07-14 18:57:23 -0300

    Bloco4 Dia02 Exercicio6

---
+++ Bloco4/Dia02/exercicio6.js
@@ -0,0 +1,10 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let impar = 0;
+
+for(let position = 0; position < numbers.length; position += 1){
+    if( numbers[position] % 2 != 0){
+        impar += 1;
+    }
+
+}
+console.log(impar);

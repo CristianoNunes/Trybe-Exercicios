609fa2b47e5685a4f99df43304b2f2c77def2e26
CristianoNunes <cristianonunes99@gmail.com>
2020-07-14 17:59:20 -0300

    Bloco4 Dia02 Exercicio2

---
+++ Bloco4/Dia02/exercicio2.js
@@ -0,0 +1,6 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let sum = 0;
+for(let position = 0; position < numbers.length; position += 1){
+    sum += numbers[position];
+}
+console.log(sum);

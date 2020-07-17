c1a54ab4c0288cda3f0e9ccfacdc27cd6b0b5e47
CristianoNunes <cristianonunes99@gmail.com>
2020-07-16 23:58:34 -0300

    Bloco4 Dia4 Exercicio5

---
+++ Bloco4/Dia04/Parte II/exercicio5.js
@@ -0,0 +1,21 @@
+let arrTeste = [2,3,2,5,8,2,3];
+let nTeste = 0;
+let contador = 0;
+let contRepetido = 0;
+let nMaisRepetido;
+
+for(let i in arrTeste){
+    nTeste = arrTeste[i]
+    for(let indice in arrTeste){
+        if(nTeste == arrTeste[indice]){
+            contador += 1;
+            
+        }
+    }
+    if(contador > contRepetido){
+        contRepetido = contador;
+        nMaisRepetido = arrTeste[i];
+    }
+    contador = 0;
+}
+console.log(nMaisRepetido);

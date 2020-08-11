93e4e5d49de32a999e1a3c857c241cc8d1d4c946
CristianoNunes <cristianonunes99@gmail.com>
2020-08-11 19:29:17 -0300

    Bloco7 Dia2 Parte II Exercicio7

---
+++ Bloco7/Dia02/ParteII/exercicio7.js
@@ -0,0 +1,27 @@
+// Crie uma função que obtenha o valor da chave de acordo com a sua posição no objeto. Por exemplo:
+// console.log(getValueByNumber(lesson1, 0));
+// Output: 'Matématica'
+
+const lesson1 = {
+  materia: 'Matemática',
+  numeroEstudantes: 20,
+  professor: 'Maria Clara',
+  turno: 'manhã',
+};
+
+const lesson2 = {
+  materia: 'História',
+  numeroEstudantes: 20,
+  professor: 'Carlos',
+};
+
+const lesson3 = {
+  materia: 'Matemática',
+  numeroEstudantes: 10,
+  professor: 'Maria Clara',
+  turno: 'noite',
+};
+
+const valueKey = (obj, indice) => Object.values(obj)[indice];
+
+console.log(valueKey(lesson1,0));

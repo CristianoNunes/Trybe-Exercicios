11ef9f1a83a8c90b34d43a1644e1f08db906bfec
CristianoNunes <cristianonunes99@gmail.com>
2020-08-11 18:43:32 -0300

    Bloco7 Dia2 Parte II Exercicio2

---
+++ Bloco7/Dia02/ParteII/exercicio2.js
@@ -0,0 +1,23 @@
+const lesson1 = {
+  materia: 'Matemática',
+  numeroEstudantes: 20,
+  professor: 'Maria Clara',
+  turno: 'manhã',
+};
+  
+const lesson2 = {
+  materia: 'História',
+  numeroEstudantes: 20,
+  professor: 'Carlos',
+};
+
+const lesson3 = {
+  materia: 'Matemática',
+  numeroEstudantes: 10,
+  professor: 'Maria Clara',
+  turno: 'noite',
+};
+
+const listKeys = (obj) => Object.keys(obj);
+  
+console.log(listKeys(lesson2));

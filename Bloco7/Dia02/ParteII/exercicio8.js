34f19036e1687c19c006a3632cb130c34ad9c673
CristianoNunes <cristianonunes99@gmail.com>
2020-08-11 19:33:10 -0300

    Bloco7 Dia2 Parte II Exercicio8

---
+++ Bloco7/Dia02/ParteII/exercicio8.js
@@ -0,0 +1,37 @@
+// Crie uma função que verifique se o par (chave / valor) existe na função. 
+// Essa função deve possuir três parâmetros, sendo eles: o objeto, o nome da 
+// chave e o valor da chave. Exemplo:
+// console.log(verifyPair(lesson3, 'turno', 'noite'));
+// Output: true,
+// console.log(verifyPair(lesson3, 'materia', 'Maria Clara'));
+// Output: false
+
+const lesson1 = {
+  materia: 'Matemática',
+  numeroEstudantes: 20,
+  professor: 'Maria Clara',
+  turno: 'manhã',
+};
+
+const lesson2 = {
+  materia: 'História',
+  numeroEstudantes: 20,
+  professor: 'Carlos',
+};
+
+const lesson3 = {
+  materia: 'Matemática',
+  numeroEstudantes: 10,
+  professor: 'Maria Clara',
+  turno: 'noite',
+};
+
+const verifyPair = (obj, key, value) => {
+  const arr = Object.entries(obj);
+  let isEqual = false;
+  for (let i in arr) {
+    if (arr[i][0] === key && arr[i][1] === value) isEqual = true;
+  }
+  return isEqual;
+};
+console.log(verifyPair(lesson3,'turno','noite'));

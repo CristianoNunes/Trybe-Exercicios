b48b95d5d2ca99c61da2af70ed9d62d21b0d0978
CristianoNunes <cristianonunes99@gmail.com>
2020-08-20 20:00:27 -0300

    Bloco8 Dia4 Exercicio 4

---
+++ Bloco8/Dia04/exercicio4.js
@@ -0,0 +1,40 @@
+const assert = require('assert')
+
+const people = [
+    {
+        name: "Nicole",
+        bornIn: 1992,
+        nationality: "Australian"
+    },
+    {
+        name: "Harry",
+        bornIn: 2008,
+        nationality: "Australian"
+    },
+    {
+        name: "Toby",
+        bornIn: 1901,
+        nationality: "Australian"
+    },
+    {
+        name: "Frida",
+        bornIn: 1960,
+        nationality: "Dannish"
+    },
+    {
+        name: "Fernando",
+        bornIn: 2001,
+        nationality: "Brazilian"
+    }
+]
+
+const bornInSec20 = year => year >= 1901 && year <= 2000;
+const nationalityAustralian = nationality => nationality === 'Australian';
+const filterPeople = (people) => people.filter(
+    ({bornIn, nationality}) => bornInSec20(bornIn) && nationalityAustralian(nationality)
+)
+
+const filteredPeople = filterPeople(people);
+
+assert.deepEqual(filteredPeople[0], { name: "Nicole", bornIn: 1992, nationality: "Australian" })
+assert.deepEqual(filteredPeople[1], { name: "Toby", bornIn: 1901, nationality: "Australian" })
